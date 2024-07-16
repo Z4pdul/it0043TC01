2a9adab49e8dd8db39b2aabd2b86bdb86e608baa
Abdullah <58437983+Z4pdul@users.noreply.github.com>
2024-07-16 19:41:21 +0800

    Update Finals.js

--- Finals/Finals.js
+++ Finals/Finals.js
@@ -1,3 +1,12 @@
+ document.addEventListener('DOMContentLoaded', () => {
+        const menuIcon = document.getElementById('menu-icon');
+        const navbar = document.querySelector('.navbar');
+
+        menuIcon.addEventListener('click', () => {
+            navbar.classList.toggle('active');
+        });
+    });
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
